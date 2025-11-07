fe905052708ff383338a74d23208e93f45906206
Sitansu Jena <sitansu.jena@transformco.com>
2025-11-07 18:39:42 +0530

    Add browser headers to bypass Cloudflare protection

--- routes/parts.js
+++ routes/parts.js
@@ -335,7 +335,19 @@ router.post('/parts/search-sears', async (req, res, next) => {
       method: 'POST',
       headers: { 
         'Content-Type': 'application/json', 
-        'x-api-key': GRAPHQL_API_KEY 
+        'x-api-key': GRAPHQL_API_KEY,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Origin': 'https://www.searspartsdirect.com',
+        'Referer': 'https://www.searspartsdirect.com/',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site'
       },
       body: JSON.stringify({
         query: `
